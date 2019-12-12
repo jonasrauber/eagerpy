94faf8c8ad567f066d38ec73db0df57b7af30546
Jonas Rauber <git@jonasrauber.de>
2019-12-12 10:43:18 +0100

    avoid importing all frameworks in astensor

--- eagerpy/astensor.py
+++ eagerpy/astensor.py
@@ -1,23 +1,26 @@
-import torch
-import tensorflow as tf
-import jax
-import numpy as np
-
 from . import PyTorchTensor
 from . import TensorFlowTensor
 from . import JAXTensor
 from . import NumPyTensor
 
 
+def _get_module_name(x):
+    # splitting is necessary for TensorFlow tensors
+    return x.__class__.__module__.split(".")[0]
+
+
 def astensor(x):
     if hasattr(x, "tensor"):
         return x
-    if isinstance(x, torch.Tensor):
+    # we use the module name instead of isinstance
+    # to avoid importing all the frameworks
+    module = _get_module_name(x)
+    if module == "torch":
         return PyTorchTensor(x)
-    if isinstance(x, tf.Tensor):
+    if module == "tensorflow":
         return TensorFlowTensor(x)
-    if isinstance(x, jax.numpy.ndarray):
+    if module == "jax":
         return JAXTensor(x)
-    if isinstance(x, np.ndarray):
+    if module == "numpy":
         return NumPyTensor(x)
     raise ValueError(f"Unknown type: {type(x)}")
