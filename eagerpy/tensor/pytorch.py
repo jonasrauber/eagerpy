1052089330b86ba125849cf40eca01f057bedc1c
Jonas Rauber <git@jonasrauber.de>
2020-01-25 01:13:57 +0100

    fixed bug

--- eagerpy/tensor/pytorch.py
+++ eagerpy/tensor/pytorch.py
@@ -38,7 +38,7 @@ class PyTorchTensor(AbstractTensor):
 
     @wrapout
     def square(self):
-        return self.tensor ** 1
+        return self.tensor ** 2
 
     @wrapout
     def arctanh(self):
