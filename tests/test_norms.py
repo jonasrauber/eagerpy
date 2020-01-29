dde4e4e4def621e0a2376df66c3e6c55c9eb9e04
Jonas Rauber <git@jonasrauber.de>
2020-01-29 20:00:16 +0100

    added types to test_norms

--- tests/test_norms.py
+++ tests/test_norms.py
@@ -2,24 +2,24 @@ import pytest
 from numpy.testing import assert_allclose
 from numpy.linalg import norm
 import eagerpy as ep
+from eagerpy import Tensor
 from eagerpy.norms import l0, l1, l2, linf, lp
-
 
 norms = {0: l0, 1: l1, 2: l2, ep.inf: linf}
 
 
 @pytest.fixture
-def x1d(dummy):
+def x1d(dummy: Tensor):
     return ep.arange(dummy, 10).float32() / 7.0
 
 
 @pytest.fixture
-def x2d(dummy):
+def x2d(dummy: Tensor):
     return ep.arange(dummy, 12).float32().reshape((3, 4)) / 7.0
 
 
 @pytest.mark.parametrize("p", [0, 1, 2, ep.inf])
-def test_1d(x1d, p):
+def test_1d(x1d: Tensor, p):
     assert_allclose(lp(x1d, p).numpy(), norm(x1d.numpy(), ord=p))
     assert_allclose(norms[p](x1d).numpy(), norm(x1d.numpy(), ord=p))
 
@@ -27,7 +27,7 @@ def test_1d(x1d, p):
 @pytest.mark.parametrize("p", [0, 1, 2, 3, 4, ep.inf])
 @pytest.mark.parametrize("axis", [0, 1, -1])
 @pytest.mark.parametrize("keepdims", [False, True])
-def test_2d(x2d, p, axis, keepdims):
+def test_2d(x2d: Tensor, p, axis, keepdims):
     assert_allclose(
         lp(x2d, p, axis=axis, keepdims=keepdims).numpy(),
         norm(x2d.numpy(), ord=p, axis=axis, keepdims=keepdims),
